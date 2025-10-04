45dc5c19ec068b9edbf1868624afcb7df24463bb
MD-KHADIMUL-ISLAM <mdki1997@gmail.com>
2025-10-04 13:17:32 +0600

    Added products

--- Program.cs
+++ Program.cs
@@ -1,9 +1,35 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
+app.MapGet("/", () =>
+{
+    var response = new { message = "This is json object", success = true };
+    return response;
+});
+
+var products = new List<Product>() {
+    new Product("Samsung s20", 1250),
+    new Product("Apple iphone 16", 1367)
+};
+app.MapGet("/products", () =>
+{
+    return Results.Ok(products);
+});
+
 
 app.Run();
+
+public record Product(string Name, decimal Price);
